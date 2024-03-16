b9fa74854a98fd9c4e9ed1df64d172370f453539
Sebastian Prentice <prentice.seb@hotmail.com>
2024-03-16 15:35:59 +0000

    finish adding changes

--- app/auth/views.py
+++ app/auth/views.py
@@ -4,11 +4,19 @@ from fastapi import APIRouter, Depends, HTTPException
 from fastapi.security.http import HTTPAuthorizationCredentials, HTTPBearer
 from starlette import status
 
+<<<<<<< Updated upstream
 from app.schemas import UnauthorizedMessage
 from app.utils import settings
+=======
+from app.schemas import TokenData
+from app.user.tasks import process_get_user
+from app.utils import settings
+
+>>>>>>> Stashed changes
 
 auth_router = APIRouter()
 
+<<<<<<< Updated upstream
 # Auth
 known_tokens = set()
 
@@ -34,3 +42,25 @@ async def get_token(
             # Assuming UnauthorizedMessage is defined somewhere in your actual code
         )
     return token
+=======
+
+
+def get_current_user(token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, settings.jwt_secret, algorithms=["HS256"])
+        username: str = payload.get("username")
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except jwt.JWTError:
+        raise credentials_exception
+    user = process_get_user(username=token_data.username)
+    if user is None:
+        raise credentials_exception
+    return user
+>>>>>>> Stashed changes
--- app/settings.py
+++ app/settings.py
@@ -7,6 +7,7 @@ class Settings(BaseSettings):
     testing: bool = False
     dev_mode: bool = False
     master_token: str = ""
+    jwt_secret: str = ""
     logfire_token: Optional[str] = None
     database_url: str = "postgresql://postgres@localhost:5432/onlybrands"
 
