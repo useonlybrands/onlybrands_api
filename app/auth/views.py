5e5532931598a0f167711ea32227c41819b8e716
Sebastian Prentice <prentice.seb@hotmail.com>
2024-03-16 15:37:52 +0000

    forgot these

--- app/auth/views.py
+++ app/auth/views.py
@@ -5,11 +5,11 @@ from starlette import status
 
 from app.schemas import TokenData
 from app.user.tasks import process_get_user
-# from app.utils import settings
+from app.utils import settings
+
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
-# known_tokens = set()
 
 
 def get_current_user(token: str = Depends(oauth2_scheme)):
@@ -19,18 +19,7 @@ def get_current_user(token: str = Depends(oauth2_scheme)):
         headers={"WWW-Authenticate": "Bearer"},
     )
     try:
-        # if settings.master_token:
-        #     known_tokens.add(settings.master_token)
-        #
-        # # Simulate a database query to find a known token
-        # if token not in known_tokens:
-        #     raise credentials_exception
-        #
-        # # If the token is a master token, return a user with all permissions
-        # if token == settings.master_token:
-        #     return User(username='master', email='master@example.com', new_user=False)
-
-        payload = jwt.decode(token, "SECRET_KEY", algorithms=["HS256"])
+        payload = jwt.decode(token, settings.jwt_secret, algorithms=["HS256"])
         username: str = payload.get("username")
         if username is None:
             raise credentials_exception
